ca8d5c090f7cfe4057fc8ec80614916a89f89d83
Ugly-318 <1194884851@qq.com>
2020-12-23 19:49:39 +0800

    删除IDEA

--- database/migrations/2020_12_23_113700_create_photos_table.php
+++ database/migrations/2020_12_23_113700_create_photos_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreatePhotosTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrationssss.
      *
      * @return void
      */
